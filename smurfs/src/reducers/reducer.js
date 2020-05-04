8bc11b97c2916bcab08572ee594a50a804a674d4
bdrummo6 <bradley.drummonds@gmail.com>
2020-05-03 20:13:47 -0400

    Setup reducer
    Imported action types from action.js, set  my inital state and completed the fetch data portion of my reducer.

--- smurfs/src/reducers/reducer.js
+++ smurfs/src/reducers/reducer.js
@@ -1,11 +1,42 @@
-
+import {
+	FETCH_DATA_START,
+	FETCH_DATA_SUCCESS,
+	FETCH_DATA_FAILURE,
+} from '../actions/actions';
 
 const initialState = {
-	smurfs: []
+	smurfs: [
+		{
+			name: '',
+			age: '',
+			height: '',
+			id: 0
+		}
+	],
+	isLoading: false,
+	error: ''
 };
 
 export const reducer = (state = initialState, action) => {
 	switch (action.type) {
+		case FETCH_DATA_START:
+			return {
+				...state,
+				isLoading: true,
+				error: ''
+			};
+		case FETCH_DATA_SUCCESS:
+			return {
+				...state,
+				isLoading: false,
+				smurfs: action.payload,
+				error: ''
+			};
+		case FETCH_DATA_FAILURE:
+			return {
+				...state,
+				error: ''
+			};
 		default:
 			return state;
 	}
