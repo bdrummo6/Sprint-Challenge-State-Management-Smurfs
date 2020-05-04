6ec2ad69796f88f1a13d287282c2fb17140b9a06
bdrummo6 <bradley.drummonds@gmail.com>
2020-05-03 22:38:09 -0400

    Update App.js

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,20 +1,35 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import { connect } from 'react-redux';
+import { getData, addData } from '../actions/actions';
+
 import "./App.css";
 
 import Smurfs from "./Smurfs";
 import NewSmurfForm from "./NewSmurfForm";
 
-class App extends Component {
+const App = props => {
 
-   render() {
-      return (
-      <div className="App">
-        <h1>Brad's SMURFS! 2.0 W/ Redux</h1>
-        <NewSmurfForm />
-        <Smurfs />
-      </div>
-    );
-  }
-}
+  const [smurf, setSmurf] = useState({ name: '', age: '', height: '' });
 
-export default App;
+  const handleChange = event => {
+    event.preventDefault();
+    setSmurf({...smurf, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit= event => {
+    event.preventDefault();
+    props.addData(smurf);
+    setSmurf({ name: '', age: '', height: '' });
+  };
+
+  return (
+    <div className="App">
+      <h1>Brad's SMURFS! 2.0 W/ Redux</h1>
+      <NewSmurfForm smurf={smurf} change={handleChange} submit={handleSubmit} />
+      <Smurfs />
+    </div>
+  )
+};
+
+
+export default connect(null, {getData, addData})(App);
